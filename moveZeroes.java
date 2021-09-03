2168ec0d93a6c53cf639ea79b6bf92075267f33f
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-03 23:13:08 +0530

    Update moveZeroes.java

--- moveZeroes.java
+++ moveZeroes.java
@@ -5,14 +5,16 @@ public class moveZeroes {
         Scanner scan = new Scanner(System.in);
         int number_of_elements = scan.nextInt();
         int[] nums = new int[number_of_elements];
+        System.out.print("[");
         for (int i = 0; i < number_of_elements; i++) {
             nums[i] = scan.nextInt();
         }
         Solution283 s = new Solution283();
         s.moveZeroes(nums);
         for (int i = 0; i < number_of_elements; i++) {
-            System.out.print(nums[i] + " ");
+            System.out.print(nums[i] + ",");
         }
+        System.out.print("[");
     }
 }
 
@@ -29,3 +31,12 @@ class Solution283 {
         }
     }
 }
+
+/*
+Testcase: 
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+ 
+Input: nums = [0]
+Output: [0]
+*/

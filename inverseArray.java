daf1c974cc9551d787c2dcaf1af741e548ba7491
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-05 22:05:46 +0530

    Add files via upload

---
+++ inverseArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class inverseArray{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number_of_elements = scan.nextInt();
+        int[] array = new int[number_of_elements];
+        for (int i = 0; i < number_of_elements; i++) {
+            array[i] = scan.nextInt();
+        }
+       
+        Solution s = new Solution();
+        s.inversefunction(array);
+       
+    }
+}
+class Solution {
+         public static void inversefunction(int[] a){
+           int[] ans = new int[a.length];
+            for(int i=0;i<a.length;i++)
+            {
+             int v = a[i];
+             ans[v]=i;
+            }
+             for (int val: ans) {
+            System.out.print(val + " ");
+        }
+         }
+}

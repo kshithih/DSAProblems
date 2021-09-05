ba2203ce93d09a06f40ae60a56a2287e2042b52f
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-05 20:47:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number_of_elements = scan.nextInt();
+        int[] array = new int[number_of_elements];
+        for (int i = 0; i < number_of_elements; i++) {
+            array[i] = scan.nextInt();
+        }
+        Solution s = new Solution();
+        s.reverseArray(array);
+        for (int val: array) {
+            System.out.print(val + " ");
+        }
+    }
+}
+class Solution {
+    public void reverseArray(int arr[]) {
+        int begin = 0;
+        int end = arr.length - 1;
+        while (begin < end) {
+            int temp = arr[begin];
+            arr[begin] = arr[end];
+            arr[end] = temp;
+            begin++;
+            end--;
+
+        }
+    }
+}

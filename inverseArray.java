97613ee5e6b8c0a1bb6a14cdcc487523f2f38689
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-05 22:06:36 +0530

    Update inverseArray.java

--- inverseArray.java
+++ inverseArray.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class inverseArray{
+class inverseArray {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int number_of_elements = scan.nextInt();
@@ -8,22 +8,21 @@ class inverseArray{
         for (int i = 0; i < number_of_elements; i++) {
             array[i] = scan.nextInt();
         }
-       
+
         Solution s = new Solution();
         s.inversefunction(array);
-       
+
     }
 }
 class Solution {
-         public static void inversefunction(int[] a){
-           int[] ans = new int[a.length];
-            for(int i=0;i<a.length;i++)
-            {
-             int v = a[i];
-             ans[v]=i;
-            }
-             for (int val: ans) {
+    public static void inversefunction(int[] a) {
+        int[] ans = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            int v = a[i];
+            ans[v] = i;
+        }
+        for (int val: ans) {
             System.out.print(val + " ");
         }
-         }
+    }
 }

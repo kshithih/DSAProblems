2bebfdc0923cdb09742ef9569e5d432e26abf575
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-06 18:58:50 +0530

    Repetition of letters in a String

---
+++ stringrepeatition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class stringrepeatition{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String question = scan.next();
+        String result = "";
+        for (int i = 0; i < question.length() - 1; i++) {
+            if (question.charAt(i) != question.charAt(i + 1)) {
+                result += question.charAt(i);
+            }
+
+        }
+        result += question.charAt(question.length() - 1);
+        System.out.println(result);
+
+    }
+}

5712c565d3081e9847649112b92e9583a16a0307
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-14 10:02:05 +0530

    Add files via upload

---
+++ findsum.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class findsum {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+        while (t > 0) {
+            int n = scan.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+
+            t--;
+            Solutions s = new Solutions();
+            System.out.println(s.findsumans(arr, n) == true ? "Yes" : "No");
+        }
+    }
+}
+
+class Solutions {
+    static boolean findsumans(int arr[], int n) {
+        //Your code here
+        int sum = 0;
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                return true;
+            }
+            sum += arr[i];
+            if (sum == 0) {
+                return true;
+            }
+            if (hs.contains(sum)) {
+                return true;
+            }
+            hs.add(sum);
+        }
+        return false;
+    }
+}

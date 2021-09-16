92dff21a6a9bbba193b58cc4be28c6d639f4ff27
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-16 14:18:23 +0530

    Add files via upload

---
+++ mergesortedsubarrays.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class mergesortedsubarrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = scan.nextInt();
+        }
+        int m = scan.nextInt();
+        int[] B = new int[m];
+        for (int i = 0; i < m; i++) {
+            B[i] = scan.nextInt();
+        }
+        int i = 0;
+        int j = 0;
+        int[] ans = new int[A.length + B.length];
+        int k = 0;
+        while (i < A.length && j < B.length) {
+            if (A[i] < B[j]) {
+                ans[k] = A[i];
+                k = k + 1;
+                i = i + 1;
+            } else if (A[i] > B[j]) {
+                ans[k] = B[j];
+                k = k + 1;
+                j = j + 1;
+            } else {
+                ans[k] = B[j];
+                k = k + 1;
+                ans[k] = A[i];
+                j = j + 1;
+                k = k + 1;
+                i = i + 1;
+            }
+        }
+        while (i < A.length) {
+            ans[k] = A[i];
+            k = k + 1;
+            i++;
+        }
+        while (j < B.length) {
+            ans[k] = B[j];
+            k = k + 1;
+            j++;
+        }
+        for (i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + "\t");
+        }
+    }
+}
+
+

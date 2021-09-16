6e40f602c0b22efdfc884dc8667227dd9fef3046
kshithih <73117520+kshithih@users.noreply.github.com>
2021-09-16 19:55:35 +0530

    Add files via upload

--- maximumsubarraysum.java
+++ maximumsubarraysum.java
@@ -4,19 +4,35 @@ public class maximumsubarraysum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int number_of_elements = scan.nextInt();
-        int[] array = new int[number_of_elements];
+        int k = scan.nextInt();
+        int[] A = new int[number_of_elements];
         for (int i = 0; i < number_of_elements; i++) {
-            array[i] = scan.nextInt();
+            A[i] = scan.nextInt();
         }
-        int maxsum = Integer.MIN_VALUE;
-        int cur = 0;
-        for (int i = 0; i < number_of_elements; i++) {
-            cur += array[i];
-            if (cur < 0) {
-                cur = 0;
+                int i=0;
+                int j =0;
+                int sum = 0;
+                int maximum =Integer.MIN_VALUE;
+                while( j<A.length){
+                    sum = sum+A[j];
+                    if((j-i+1) < k){
+
+                        j++;
+
+                    }
+                    else if((j-i +1 )== k){
+                        maximum = Math.max(sum ,maximum );
+                        sum -= A[i];
+                        j++;
+                        i++;
+
+                    }
+                }
+        System.out.println(maximum);;
             }
         }
-        System.out.println(cur);
-    }
-}
+
+
+
+
 

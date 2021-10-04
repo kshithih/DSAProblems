3d99fb90823eb76514c1b6d432aa4834998cfb7f
kshithih <73117520+kshithih@users.noreply.github.com>
2021-10-04 10:33:04 +0530

    Update sumofnumbersinString.java

--- sumofnumbersinString.java
+++ sumofnumbersinString.java
@@ -1,3 +1,14 @@
+// //Program to find the sum of all numbers present in the string is discussed here. Given a string containing both numbers and alphabets, the task is to find the sum of all numbers present in the string.
+// For example, consider the following :
+// Input:
+// abc45def5ghi32
+// Output:
+// 82
+
+// Explanation:
+// The numbers 45, 5 and 32 are present in the string.
+// Sum = 45 + 5 + 32 = 82.
+
 import java.util.Scanner;
 
 public class sumofnumbersinString {
@@ -9,20 +20,20 @@ public class sumofnumbersinString {
 
     public static void diamond(String s) {
         StringBuilder sb = new StringBuilder();
-        int sum =0;
-        String a ="0";
-        for (int i=0;i<s.length();i++){
-            if(Character.isDigit(s.charAt(i))){
+        int sum = 0;
+        String a = "0";
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
 
-                a = a+s.charAt(i);
-            }else{
-                if(a!=null) {
+                a = a + s.charAt(i);
+            } else {
+                if (a != null) {
                     sum = sum + Integer.parseInt(a);
-                    a ="0";
+                    a = "0";
                 }
             }
         }
-        sum +=  Integer.parseInt(a);
+        sum += Integer.parseInt(a);
         System.out.println(sum);
-        }
     }
+}

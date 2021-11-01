3106d370538d3570ab5dd4badc848d4166cf5c0a
kshithih <73117520+kshithih@users.noreply.github.com>
2021-11-01 14:12:33 +0530

    Update longestSubstringPalindrome.java

--- longestSubstringPalindrome.java
+++ longestSubstringPalindrome.java
@@ -1,25 +1,25 @@
+import java.util.Scanner;
 
-
-import java.util.*;
 class longestSubstringPalindrome {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.next();
         System.out.println(longestPalindromicSubstring(s));
     }
-    public static  String longestPalindromicSubstring(String A) {
+
+    public static String longestPalindromicSubstring(String A) {
         String ans = "";
         String res = "";
-        if(A.length() == 1){
+        if (A.length() == 1) {
             return A;
         }
         int max = Integer.MIN_VALUE;
-        for (int i=0;i<A.length();i++){
-            for (int j=i+1;j<=A.length();j++){
-                String n = A.substring(i,j);
-            if(isPalindrome(n  , i , j )){
-                    ans =  A.substring(i,j);
-                    if(ans.length() > max){
+        for (int i = 0; i < A.length(); i++) {
+            for (int j = i + 1; j <= A.length(); j++) {
+                String n = A.substring(i, j);
+                if (isPalindrome(n, i, j)) {
+                    ans = A.substring(i, j);
+                    if (ans.length() > max) {
                         max = ans.length();
                         res = ans;
                     }
@@ -29,14 +29,15 @@ class longestSubstringPalindrome {
         }
         return res;
     }
-    public static boolean isPalindrome(String a , int i , int j){
+
+    public static boolean isPalindrome(String a, int i, int j) {
         int k = 0;
-        int l = a.length()-1;
-        while(k<l){
-            if(a.charAt(k) == a.charAt(l)){
+        int l = a.length() - 1;
+        while (k < l) {
+            if (a.charAt(k) == a.charAt(l)) {
                 k++;
                 l--;
-            }else {
+            } else {
                 return false;
             }
         }

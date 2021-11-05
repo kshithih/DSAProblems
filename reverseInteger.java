6e2410f106904b48cdede584cc80c1243942545c
kshithih <73117520+kshithih@users.noreply.github.com>
2021-11-05 14:37:05 +0530

    Update reverseInteger.java

--- reverseInteger.java
+++ reverseInteger.java
@@ -1,9 +1,33 @@
+/*You are given an integer N and the task is to reverse the digits of the given integer. Return 0 if the result overflows and does not fit in a 32 bit signed integer
+Look at the example for clarification.
+Problem Constraints
+N belongs to the Integer limits.
+
+Input Format
+Input an Integer.
+
+Output Format
+Return a single integer denoting the reverse of the given integer.
+
+Example Input
+Input 1:
+ x = 123
+Input 2:
+ x = -123
+Example Output
+Output 1:
+ 321
+Ouput 2:
+ -321
+*/
+
+
 import java.util.Scanner;
 
 class reverseInteger{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+        int number = scan.nextInt();
         System.out.println(reverse(a));
     }
 
